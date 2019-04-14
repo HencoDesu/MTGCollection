7338ebc196a62092c5ed0e9dae0b678a40905c09
Footh <Footh@DESKTOP-IU56SS3>
2019-04-14 20:26:37 +0300

    CardCollection in own View

--- View/MainWindow.xaml.cs
+++ View/MainWindow.xaml.cs
@@ -9,25 +9,5 @@ namespace HencoDesu.MtgCollection.View {
 		public MainWindow() {
 			InitializeComponent();
 		}
-
-		private void RaphWindow_OnSizeChanged(object sender, SizeChangedEventArgs e) {
-			CardsGrid.Rows = (int)Math.Floor(CardsGrid.ActualHeight / 360);
-			CardsGrid.Columns = (int)Math.Floor(CardsGrid.ActualWidth / 220);
-
-			if (CardsGrid.Children.Count != CardsGrid.Rows * CardsGrid.Columns) {
-				CardsGrid.Children.Clear();
-
-				var index = 0;
-				for (var row = 0; row < CardsGrid.Rows; row++) {
-					for (var column = 0; column < CardsGrid.Columns; column++) {
-						var CardView = new Card {
-							Margin = new Thickness(5),
-							DataContext = (DataContext as ViewModel.MainWindow).Cards[index++]
-						};
-						CardsGrid.Children.Add(CardView);
-					}
-				}
-			}
-		}
 	}
 }
