9bb82264910a6e5e6ebabdf6c31fc99d440c86c1
Footh <Footh@DESKTOP-IU56SS3>
2019-04-14 18:45:01 +0300

    Grid adapting

--- View/MainWindow.xaml.cs
+++ View/MainWindow.xaml.cs
@@ -1,10 +1,18 @@
-﻿namespace HencoDesu.MtgCollection.View {
+﻿using System;
+using System.Windows;
+
+namespace HencoDesu.MtgCollection.View {
 	/// <summary>
 	/// Interaction logic for MainWindow.xaml
 	/// </summary>
 	public partial class MainWindow {
 		public MainWindow() {
 			InitializeComponent();
+		}
+
+		private void RaphWindow_OnSizeChanged(object sender, SizeChangedEventArgs e) {
+			CardsGrid.Rows = (int)Math.Floor((Height - 32) / 360);
+			CardsGrid.Columns = (int)Math.Floor(Width / 220);
 		}
 	}
 }
--- ViewModel/MainWindow.cs
+++ ViewModel/MainWindow.cs
@@ -1,9 +1,10 @@
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using HencoDesu.RaphApi.WPF.ViewModel;
 
 namespace HencoDesu.MtgCollection.ViewModel {
 	public class MainWindow : RaphViewModel{
-		public ObservableCollection<Card> Cards { get; } = new ObservableCollection<Card>() {
+		public IEnumerable<Card> Cards { get; } = new ObservableCollection<Card>() {
 			new Card(455080),
 			new Card(454913),
 			new Card(454974),
