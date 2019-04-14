5f2754e761d167e5a10af975510ddb572aa8941e
Footh <Footh@DESKTOP-IU56SS3>
2019-04-14 19:18:48 +0300

    AutoGrid

--- View/MainWindow.xaml.cs
+++ View/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Windows;
+using System.Windows.Data;
 
 namespace HencoDesu.MtgCollection.View {
 	/// <summary>
@@ -11,8 +12,23 @@ namespace HencoDesu.MtgCollection.View {
 		}
 
 		private void RaphWindow_OnSizeChanged(object sender, SizeChangedEventArgs e) {
-			CardsGrid.Rows = (int)Math.Floor((Height - 32) / 360);
-			CardsGrid.Columns = (int)Math.Floor(Width / 220);
+			CardsGrid.Rows = (int)Math.Floor(CardsGrid.Height / 360);
+			CardsGrid.Columns = (int)Math.Floor(CardsGrid.Width / 220);
+
+			if (CardsGrid.Children.Count != CardsGrid.Rows * CardsGrid.Columns) {
+				CardsGrid.Children.Clear();
+
+				var index = 0;
+				for (var row = 0; row < CardsGrid.Rows; row++) {
+					for (var column = 0; column < CardsGrid.Columns; column++) {
+						var CardView = new Card {
+							Margin = new Thickness(5),
+							DataContext = (DataContext as ViewModel.MainWindow).Cards[index++]
+						};
+						CardsGrid.Children.Add(CardView);
+					}
+				}
+			}
 		}
 	}
 }
--- ViewModel/MainWindow.cs
+++ ViewModel/MainWindow.cs
@@ -4,13 +4,25 @@ using HencoDesu.RaphApi.WPF.ViewModel;
 
 namespace HencoDesu.MtgCollection.ViewModel {
 	public class MainWindow : RaphViewModel{
-		public IEnumerable<Card> Cards { get; } = new ObservableCollection<Card>() {
+		public IList<Card> Cards { get; } = new ObservableCollection<Card> {
 			new Card(455080),
 			new Card(454913),
 			new Card(454974),
 			new Card(454975),
 			new Card(454981),
 			new Card(454947),
+			new Card(435152),
+			new Card(437266),
+			new Card(437423),
+			new Card(437335),
+			new Card(437247),
+			new Card(437319),
+			new Card(437249),
+			new Card(437250),
+			new Card(459399),
+			new Card(459367),
+			new Card(459217),
+			new Card(459372),
 		};
 	}
 }
